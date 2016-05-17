b8f29f04bf8d22f9cd16f5fe7861936e43d60b4d
Rylie Hanson <halfbrain27@gmail.com>
2016-05-16 21:15:45 -0700

    Draw some grids.

--- practice.py
+++ practice.py
@@ -44,4 +44,38 @@ def box4(sides, size):
         Fred.forward(size)
         Fred.left(360/sides)
     turtle.done()
-box4(7,125)
+
+# fuckin draws a grid of dots using a loop in a loop
+def grid1():
+    Fred = turtle.Turtle()
+    for row in range(10):
+        #this loop draws the row
+        for column in range(10):
+            Fred.dot()
+            Fred.penup()
+            Fred.forward(20)
+        Fred.back(20 * 10)
+        Fred.right(90)
+        Fred.forward(20)
+        Fred.left(90)
+    turtle.done()
+
+# draws a grid of fuckin dots
+# uses parameters (rows,dots,ect) instead of values
+# size means distance between dots
+def grid2(rows,dots,size = 40):
+    Fred = turtle.Turtle()
+    for row in range(rows):
+        #this loop draws the row
+        for column in range(dots):
+            Fred.dot()
+            # the penup command can be before the loop
+            Fred.penup()
+            Fred.forward(size)
+        # gets Fred in place for the next row
+        Fred.back(size * dots)
+        Fred.right(90)
+        Fred.forward(size)
+        Fred.left(90)
+    turtle.done()
+grid2(20,15,10)
